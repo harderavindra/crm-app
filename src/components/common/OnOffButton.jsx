bca8393706b7149069847020827e9823a95e6bfa
ravi <ravindra@stavyah.com>
2025-02-14 03:10:05 +0530

    Dashboard Green color class name changed

--- src/components/common/OnOffButton.jsx
+++ src/components/common/OnOffButton.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const OnOffButton = () => {
     const [isActive, setIsActive] = useState(false)
   return (
-    <button className={`${ !isActive? 'bg-gray-200 justify-start': 'bg-primary justify-end'} rounded-full h-auto w-9 flex p-[2px] cursor-pointer `} onClick={()=>setIsActive(!isActive)}>
+    <button className={`${ !isActive? 'bg-gray-200 justify-start': 'bg-brand-green justify-end'} rounded-full h-auto w-9 flex p-[2px] cursor-pointer `} onClick={()=>setIsActive(!isActive)}>
         <span className='w-4 h-4 bg-white inline-block rounded-full'></span></button>
   )
 }
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -37,12 +37,12 @@ const Dashboard = () => {
                 <div className='w-full flex flex-row gap-4'>
                     <Card heading={'Recent Sales'}>
                         <div className='flex flex-col gap-3 mt-4 relative '>
-                            <span className='w-[2px]  mb-2 absolute top-5 left-[7px] bg-primary rounded-full' style={{ height: 'calc(100% - 50px)' }}></span>
+                            <span className='w-[2px]  mb-2 absolute top-5 left-[7px] bg-brand-green rounded-full' style={{ height: 'calc(100% - 50px)' }}></span>
                             {recentSalesData.map((sale)=>((
                             <div className='bg-gray-50 relative ml-8 px-3 py-2 rounded-sm'>
                                 <div className='flex  justify-between font-semibold'><p>{sale.title}</p><span>{sale.value}</span></div>
                                 <p className='text-xs text-gray-400'>{sale.timeSpam}</p>
-                                <span className='bg-primary w-2 h-2 rounded-full absolute -left-7 top-5'></span>
+                                <span className='bg-brand-green w-2 h-2 rounded-full absolute -left-7 top-5'></span>
                             </div>
                             )))
                         }
@@ -53,12 +53,12 @@ const Dashboard = () => {
                         <Card heading={'Leads'}>
                             <div className='mt-4'>
                                 <h4 className='flex justify-between'>IT Support Services - Sep 27th <OnOffButton /></h4>
-                                <div className='bg-gray-100 mt-3 rounded-full h-2  flex '><div className='bg-primary rounded-full h-2  flex ' style={{ width: '20%' }}></div></div>
+                                <div className='bg-gray-100 mt-3 rounded-full h-2  flex '><div className='bg-brand-green rounded-full h-2  flex ' style={{ width: '20%' }}></div></div>
                                 <p className='text-xs font-medium text-gray-400'>Review Requirements and Approve Proposal </p>
                             </div>
                             <div className='mt-3'>
                                 <h4 className='flex justify-between'>IT Support Services - Sep 27th <OnOffButton /></h4>
-                                <div className='bg-gray-100 mt-3 rounded-full h-2  flex '><div className='bg-primary rounded-full h-2  flex ' style={{ width: '20%' }}></div></div>
+                                <div className='bg-gray-100 mt-3 rounded-full h-2  flex '><div className='bg-brand-green rounded-full h-2  flex ' style={{ width: '20%' }}></div></div>
                                 <p className='text-xs font-medium text-gray-400'>Review Requirements and Approve Proposal </p>
                             </div>
                         </Card>
@@ -94,17 +94,17 @@ const Dashboard = () => {
                         <Card heading={'Leads'}>
                             <div className='mt-4 border-b border-gray-300 pb-3'>
                                 <h4 className='flex justify-between'>IT Support Services - Sep 27th <OnOffButton /></h4>
-                                <div className='bg-gray-100 mt-3 rounded-full h-2  flex mb-1 '><div className='bg-primary rounded-full h-2  flex ' style={{ width: '20%' }}></div></div>
+                                <div className='bg-gray-100 mt-3 rounded-full h-2  flex mb-1 '><div className='bg-brand-green rounded-full h-2  flex ' style={{ width: '20%' }}></div></div>
                                 <p className='text-xs font-medium text-gray-400'>Review Requirements and Approve Proposal </p>
                             </div>
                             <div className='mt-4 border-b border-gray-300 pb-3'>
                                 <h4 className='flex justify-between'>IT Support Services - Sep 27th <OnOffButton /></h4>
-                                <div className='bg-gray-100 mt-3 rounded-full h-2  flex mb-1 '><div className='bg-primary rounded-full h-2  flex ' style={{ width: '20%' }}></div></div>
+                                <div className='bg-gray-100 mt-3 rounded-full h-2  flex mb-1 '><div className='bg-brand-green rounded-full h-2  flex ' style={{ width: '20%' }}></div></div>
                                 <p className='text-xs font-medium text-gray-400'>Review Requirements and Approve Proposal </p>
                             </div>
                             <div className='mt-4 border-b border-gray-300 pb-3'>
                                 <h4 className='flex justify-between'>IT Support Services - Sep 27th <OnOffButton /></h4>
-                                <div className='bg-gray-100 mt-3 rounded-full h-2  flex mb-1 '><div className='bg-primary rounded-full h-2  flex ' style={{ width: '20%' }}></div></div>
+                                <div className='bg-gray-100 mt-3 rounded-full h-2  flex mb-1 '><div className='bg-brand-green rounded-full h-2  flex ' style={{ width: '20%' }}></div></div>
                                 <p className='text-xs font-medium text-gray-400'>Review Requirements and Approve Proposal </p>
                             </div>
                         </Card>
