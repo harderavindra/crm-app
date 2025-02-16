c6078a1db3efcbbc5984f4631033d2aafc3efa12
ravi <ravindra@stavyah.com>
2025-02-17 00:02:43 +0530

    SearchContactPage router error

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -8,8 +8,8 @@ import AiAgentPage from '../pages/AiAgentPage'
 import LeadsPage from '../pages/LeadsPage'
 import InboxPage from '../pages/InboxPage'
 import AddProjectPage from '../pages/AddProjectPage'
-import SearchContactPage from '../pages/searchContactPage'
 import PendingApprovalPage from '../pages/PendingApprovalPage'
+import SearchContactPage from '../pages/searchContactPage'
 
 const AppRouter = () => {
   return (
@@ -24,7 +24,7 @@ const AppRouter = () => {
           <Route path="/leads" element={<LeadsPage />} />
           <Route path="/inbox" element={<InboxPage />} />
           <Route path="/addProject" element={<AddProjectPage />} />
-          <Route path="/searchContact" element={<SearchContactPage />} />
+          <Route path="/searchContact" element={<SearchContactPage/>} />
           <Route path="/pendingApproval" element={<PendingApprovalPage />} />
         </Route>
       </Routes>
